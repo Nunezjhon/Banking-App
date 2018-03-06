96f573bea4e24b35e592987496dfa5405a3fbd8e
Jhon Nunez <JHON@123.local>
2018-03-05 19:40:22 -0600

    Banking App Files

--- customer.h
+++ customer.h
@@ -3,7 +3,8 @@
 //
 // Customer class for banking program in modern C++.
 //
-// << YOUR NAME >>
+// Jhon Nunez
+// jnunez34
 // U. of Illinois, Chicago
 // CS 341: Spring 2018
 // Project 01
@@ -20,8 +21,55 @@ using namespace std;
 class Customer
 {
 
-  // 
-  // TODO
-  //
+ private:
+    string Name;
+    int ID;
+    double Balance;
+  
+  public:
+  
+  Customer(string name, int id, double balance)
+    : Name(name), ID(id), Balance(balance)
+    {}
+    
+    string getName() {
+      return Name;
+    }
+    
+    int getID() {
+      return ID;
+    }
+  
+    double getBalance() {
+      return Balance;
+    }
+
+    double transactionMod(double x) {
+      return Balance += x;
+    }
+  
+};
+
+class Transaction
+{
+  
+  private:
+    int ID;
+    double userTransaction;
+    
+  public:
+  
+  Transaction(int id, double utrans)
+  : ID(id), userTransaction(utrans)
+  {}
+  
+  int getID(){
+    return ID;
+  }
+  
+  double getUserTransaction(){
+    return userTransaction;
+  }
+
   
 };
